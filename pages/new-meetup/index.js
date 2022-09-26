425e5261c280f1d050ad47d11b1ee0d10c61cf21
Rishabh Kaushal <kaushal.rishabh4@gmail.com>
2022-09-26 13:29:28 +0530

    Initialized Next JS project for Full stack implementation

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,11 @@
+// Our-domain.com/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+function NewMeetupPage() {
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage;
